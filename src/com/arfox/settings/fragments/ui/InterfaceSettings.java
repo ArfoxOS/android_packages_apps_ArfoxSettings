779f16e11f4840a506b611781256b10e51a45c0e
DylanAkp <github@dylanakp.dev>
2025-05-22 22:42:07 +0200

    ArfoxSettings: Add Expressive Design switch

--- src/com/arfox/settings/fragments/ui/InterfaceSettings.java
+++ src/com/arfox/settings/fragments/ui/InterfaceSettings.java
@@ -4,16 +4,25 @@ import android.content.Intent;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
+import androidx.preference.SwitchPreference;
 import android.os.Bundle;
+import android.os.SystemProperties;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-
 public class InterfaceSettings extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_EXPRESSIVE_DESIGN = "expressive_design";
+    private static final String PROP_EXPRESSIVE_DESIGN = "persist.sys.arfox.is_expressive_design_enabled";
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.getKey().equals(KEY_EXPRESSIVE_DESIGN)) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(PROP_EXPRESSIVE_DESIGN, value ? "1" : "0");
+            return true;
+        }
         return false;
     }
 
@@ -25,7 +34,12 @@ public class InterfaceSettings extends SettingsPreferenceFragment implements Pre
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.interface_settings, rootKey);
-
         getActivity().setTitle(R.string.arfox_interface_title);
+
+        SwitchPreference expressiveDesign = findPreference(KEY_EXPRESSIVE_DESIGN);
+        if (expressiveDesign != null) {
+            expressiveDesign.setChecked(SystemProperties.getBoolean(PROP_EXPRESSIVE_DESIGN, false));
+            expressiveDesign.setOnPreferenceChangeListener(this);
+        }
     }
 }
